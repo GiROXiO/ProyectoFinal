dee7a0880abf8636a5982b88ad152fc688dc2944
Santiago <scarta@uninorte.edu.co>
2025-04-18 22:42:13 -0500

    segundo commit

--- scripts/game_over_scene.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_continue_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_go_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
--- scripts/main.gd
+++ scripts/main.gd
@@ -113,6 +113,3 @@ func deleteSave(slot: int):
 
 func showInfo(text: String):
 	$InfoLabel.text = text
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
