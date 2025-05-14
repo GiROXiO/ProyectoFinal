06df73b74ef9b93bd9e4bd85c4fda43ac92440ee
Jeronimo <jecastroam@gmail.com>
2025-05-14 15:54:47 -0500

    Modified gitignore

--- scripts/gui/inventory_gui.gd
+++ scripts/gui/inventory_gui.gd
@@ -2,11 +2,15 @@ extends Control
 
 signal opened
 signal closed
+signal onInventory
 
 var isOpen: bool = false
+var on_inventory: bool = false
 
 @onready var inventory: Inventory = preload("res://resources/inventoryResources/playerInventory.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var slots_gui: Array = $NinePatchRect/GridContainer.get_children()
+@onready var slots: Array = inventory.slots
+@onready var dragPreview:= $DragPreview
 
 func _ready():
 	close()
@@ -15,7 +19,8 @@ func _ready():
 
 func update():
 	for i in range(min(inventory.slots.size(), slots.size())):
-		slots[i].update(inventory.slots[i])
+		slots[i].index = i
+		slots_gui[i].update(inventory.slots[i])
 
 func open():
 	visible = true
@@ -27,6 +32,11 @@ func close():
 	isOpen = false
 	closed.emit()
 
-
 func _on_back_to_start_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
+
+func _on_mouse_entered() -> void:
+	print("On inventory")
+
+func _on_mouse_exited() -> void:
+	print("Not on inventory")
--- scripts/gui/slot_gui.gd
+++ scripts/gui/slot_gui.gd
@@ -1,8 +1,18 @@
 extends Panel
 
+class_name SlotGui
+
 @onready var backgroundSprite: Sprite2D = $background
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/item
 @onready var amountLabel: Label = $CenterContainer/Panel/Label
+
+var onSlot: bool = false
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	pass
 
 func update(slot: InventorySlot):
 	if !slot.item:
@@ -13,3 +23,14 @@ func update(slot: InventorySlot):
 		itemSprite.texture = slot.item.texture
 		amountLabel.visible = slot.amount>1
 		amountLabel.text = str(slot.amount)
+
+func dragging(index: int):
+	pass
+
+func _on_mouse_entered() -> void:
+	print("On slot")
+	onSlot = true
+
+func _on_mouse_exited() -> void:
+	print("Not on slot")
+	onSlot = false
