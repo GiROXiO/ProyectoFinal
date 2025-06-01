d037ff08d485ce986b7e712bcd62afa417c5dd50
Santiago <scarta@uninorte.edu.co>
2025-06-01 04:15:49 -0500

    Haciendo correciones

--- scripts/options.gd
+++ scripts/options.gd
@@ -6,26 +6,38 @@ extends Control
 @onready var mon = $MarginContainer/GridContainer/BtnFilterContainer/btnFilter3
 @onready var optionBus = OptionsBus
 
-func _on_back_pressed() -> void:
-	self.visible = false
+#func _on_back_pressed() -> void:
+	#self.visible = false
 
 func _on_btn_filter_1_pressed() -> void:
 	print("Boton de Protonopia presionado")
 	OptionsBus.current_filter = "protanopia"
+	get_tree().paused = false
 	print(OptionsBus.current_filter)
 
 func _on_btn_filter_2_pressed() -> void:
 	print("Boton de Tritanopia presionado")
 	OptionsBus.current_filter = "tritanopia"
+	get_tree().paused = false
 	print(OptionsBus.current_filter)
 
 func _on_btn_filter_3_pressed() -> void:
 	print("Boton de Monocromatismo presionado")
 	OptionsBus.current_filter = "monocromatismo"
+	get_tree().paused = false
 	print(OptionsBus.current_filter)
 
 
 func _on_btn_filter_4_pressed() -> void:
 	print("Boton de apagado de filtro presionado")
 	OptionsBus.current_filter = ""
+	get_tree().paused = false
 	print(OptionsBus.current_filter)
+
+
+func _on_back_button_up() -> void:
+	get_tree().paused = true
+
+
+func _on_back_button_down() -> void:
+	self.visible = false
