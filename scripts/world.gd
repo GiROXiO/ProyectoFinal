3428afc73c566933fd64e7a378522bbb09229372
Santiago <scarta@uninorte.edu.co>
2025-06-03 23:39:56 -0500

    Metiendo porcentaje de misiones

--- scripts/world.gd
+++ scripts/world.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var player = $Player
 @onready var inventory = $GUI/InventoryGUI
 @onready var healthBar = $GUI/HeatlhBar
+@onready var misionCompleted = $GUI/MissionsCompleted
 
 func _ready():
 	inventory.opened.connect(_on_inventory_opened)
@@ -18,6 +19,8 @@ func _ready():
 
 func _process(_delta) -> void:
 	healthBar.value = player.get_life()
+	global.misionCompleted = snapped(get_misions_completed() / 8 * 100, 0.01)
+	misionCompleted.text = str(global.misionCompleted, "%") 
 	self._filter_changed(OptionsBus.current_filter)
 	playMusic()
 
@@ -61,3 +64,23 @@ func playMusic() -> void:
 	if not get_node("/root/World/bg").playing and not get_node("/root/World/bg2").playing and not get_node("/root/World/bg3").playing and not get_node("/root/World/bg4").playing and not get_node("/root/World/bg5").playing and not get_node("/root/World/bg6").playing:
 		var index = randi() % songs.size()
 		songs[index].play()
+
+func get_misions() -> Array[bool]:
+	var misions: Array[bool]
+	misions.append(Dialogic.VAR.MissionAcepted.Luis_Mission.luis_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Maritza_Mision.maritza_mission_completed)
+	misions.append( Dialogic.VAR.MissionAcepted.Mono_Mission.mono_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Ponllo_Mission.ponllo_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Gabriella_Mission.gabriella_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Ignacio_Mission.ignacio_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Gabriella_Mission2.gabriella_mission_completed)
+	misions.append(Dialogic.VAR.MissionAcepted.Angel_Mission.angel_mission_completed)
+	return misions
+	
+func get_misions_completed() -> float:
+	var misions: Array[bool] = get_misions()
+	var completed: float = 0
+	for mision in misions:
+		if(mision):
+			completed += 1
+	return completed
